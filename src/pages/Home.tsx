eacfc831be3cf9c78df42f8ef2b77d24c85a2a23
a.mahkam.950 <a.mahkam.9500@gmail.com>
2025-02-27 11:41:44 +0330

    add resume page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import { NavLink } from "react-router";
+
+const Home: React.FC<PageProps> = () => {
+  return (
+    <main className="bg-black">
+      <div className="flex justify-center items-center w-full h-dvh background max-[1100px]:p-5">
+        <div className="p-10 bg-black backdrop-blur-[5px] bg-opacity-30 rounded-[60px] border border-gray-600 flex flex-col justify-center items-center animation shadow-2xl shadow-black max-[1100px]:h-full max-[1100px]:justify-between">
+          <div className="border-2 rounded-full p-2 hover:p-3 duration-300 animation">
+            <img
+              className="w-40 h-40 rounded-full hover:w-44 hover:h-44 duration-300"
+              src="/img/me.jpg"
+            />
+          </div>
+          <div className="text-center pt-10">
+            <h1 className="text-3xl font-bold  text-[#16B6BB] mb-5 animation2">
+              Abolfazl Mahkam
+            </h1>
+
+            <p className="text-white">Front-End Developer</p>
+            <p className="text-white">Computer engineering student</p>
+            <p className="text-white">Qom, Islamic Republic of Iran</p>
+          </div>
+
+          <div>
+            <NavLink
+              to={"/resume"}
+              className="flex justify-center items-center mt-10 px-5 py-3 duration-300 rounded-lg shadowHover"
+            >
+              <span className="text-[#EC3710] font-semibold text-lg">
+                Resume
+              </span>
+            </NavLink>
+          </div>
+
+          <div>
+            <div className="pt-10">
+              <span className="social-media-buttons">
+                <a
+                  className="dokme"
+                  href="https://instagram.com/a.mahkam.950"
+                  target="_blank"
+                >
+                  <span className="social-media-button insta">
+                    <img alt="Instagram" src="/img/Insta.png" />
+                  </span>
+                </a>
+                <a
+                  className="dokme"
+                  href="https://github.com/AbolfazlMahkam"
+                  target="_blank"
+                >
+                  <span className="social-media-button github">
+                    <img alt="GitHub" src="/img/Github.png" />
+                  </span>
+                </a>
+                <a
+                  className="dokme"
+                  href="https://www.linkedin.com/in/abolfazl-mahkam-ab26442a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                  target="_blank"
+                >
+                  <span className="social-media-button link">
+                    <img alt="Linkedin" src="/img/linkedin.png" />
+                  </span>
+                </a>
+                <a
+                  className="dokme"
+                  href="https://a.mahkam.9500@gmail.com"
+                  target="_blank"
+                >
+                  <span className="social-media-button email">
+                    <img alt="Email" src="/img/email.png" />
+                  </span>
+                </a>
+                <a
+                  className="dokme"
+                  href="https://twitter.com/amahkam950"
+                  target="_blank"
+                >
+                  <span className="social-media-button X">
+                    <img alt="Twitter(X)" src="/img/X.png" />
+                  </span>
+                </a>
+                <a
+                  className="dokme"
+                  href="https://telegram.me/a_mahkam_950"
+                  target="_blank"
+                >
+                  <span className="social-media-button tel">
+                    <img alt="Telegram" src="/img/telegram.png" />
+                  </span>
+                </a>
+                <a
+                  className="dokme"
+                  href="https://eitaa.com/a_mahkam_950"
+                  target="_blank"
+                >
+                  <span className="social-media-button eitaa">
+                    <img alt="Eitaa" src="/img/eitaa.png" />
+                  </span>
+                </a>
+                <a className="dokme" href="" target="_blank">
+                  <span className="social-media-button discord">
+                    <img alt="discord" src="/img/discord.png" />
+                  </span>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
+
+export const Head: HeadFC = () => <title>Home Page</title>;
---
+++ src/pages/Resume.tsx
@@ -0,0 +1,368 @@
+// https://drive.google.com/file/d/1WDTe1JzC5Mcev3UWkZwhOLBvC8-a0paI/view?usp=sharing
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+
+const Resume: React.FC<PageProps> = () => {
+  return (
+    <main>
+      <div className="absolute left-2/3 top-5 w-36 max-[1100px]:left-5">
+        <a
+          className="p-2 bg-red-500 text-white font-bold no-underline rounded-md duration-300 hover:bg-red-700 hover:shadow-xl"
+          href="https://drive.google.com/file/d/1WDTe1JzC5Mcev3UWkZwhOLBvC8-a0paI/view?usp=sharing"
+          target="_blank"
+        >
+          Download PDF!
+        </a>
+      </div>
+      <div className="block font-sans">
+        <header>
+          <div>
+            <div className="w-1/2 h-14 border-r-2 border-black"></div>
+            <h1 className="text-4xl max-[1100px]:text-3xl font-mono text-center pt-2 josefinsans text-red-600">
+              <strong>Abolfazl Mahkam</strong>
+            </h1>
+          </div>
+          <div className="flex flex-row pt-3 pb-8">
+            <div className="flex-1 h-4  mr-2">
+              <div className="border-b-2 border-red-700 w-full h-full"></div>
+            </div>
+            <h2 className="flex-1 text-lg font-sans">FrontEnd Developer</h2>
+          </div>
+        </header>
+        <main className="grid grid-cols-6 px-80 max-[1100px]:px-0 max-[1100px]:grid-cols-1">
+          <div className="sidebar hidden max-[1100px]:block">
+            <div className="flex justify-center p-5">
+              <div className="rounded-lg border-2 border-red-700 p-1 hover:p-2 duration-300">
+                <img
+                  src="/img/me.jpg"
+                  className="aspect-auto rounded-md hover:shadow-xl shadow-black duration-300"
+                />
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-1 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">CONTACT</h3>
+                <ul className="dot-margin list-disc list-inside">
+                  <li className="py-1 px-5">
+                    <a
+                      className="underline"
+                      href="tel:+989104531958"
+                      target="_blank"
+                    >
+                      (+98)9104531958
+                    </a>
+                  </li>
+                  <li className="py-1 px-5">
+                    <a
+                      className="underline"
+                      href="mailto:a.mahkam.9500@gmail.com"
+                      target="_blank"
+                    >
+                      a.mahkam.9500@gmail.com
+                    </a>
+                  </li>
+
+                  <li className="flex flex-row justify-around py-3 px-5">
+                    <a
+                      href="https://github.com/AbolfazlMahkam/"
+                      target="_blank"
+                    >
+                      <img src="/img/github.png" width={25} />
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/in/abolfazl-mahkam/"
+                      target="_blank"
+                    >
+                      <img src="/img/linkedin.png" width={25} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">SKILLS</h3>
+                <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                  <li className="py-1 px-5">
+                    Full proficiency in HTML, CSS, JavaScript and TypeScript.
+                  </li>
+                  <li className="py-1 px-5">
+                    Proficiency ReactJS, NextJS, VueJS and NuxtJS.
+                  </li>
+                  <li className="py-1 px-5">
+                    Skilled in TailwindCSS, Bootstrap, and responsive design
+                    principles.
+                  </li>
+                  <li className="py-1 px-5">
+                    Proficient in Node.js and MongoDB.
+                  </li>
+                  <li className="py-1 px-5">
+                    Proficiency in Git, Figma, AdobeXD, and collaborative
+                    workflows.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">LANGUAGES</h3>
+                <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                  <li className="py-1 px-5">Persian - Native</li>
+                  <li className="py-1 px-5">English - Intermediate(B1)</li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">EDUCATION</h3>
+                <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                  <div className="p-4 pl-6">
+                    <h1 className="font-semibold text-xl">
+                      Isalmic Azad University
+                    </h1>
+                    <h4 className="font-extralight">Oct 2023 - present</h4>
+                    <h3>
+                      Pursuing a Bachelor’s Degree in Computer Engineering
+                    </h3>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className=" col-span-4">
+            <div className="px-10 max-[1100px]:px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-1 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">ABOUT ME</h3>
+                <p className="px-4 text-sm pb-4 text-justify">
+                  I am a dedicated Front-End Developer with a passion for
+                  building responsive, user-friendly websites and web
+                  applications. Proficient in HTML, CSS, JavaScript, TypeScript,
+                  and modern frameworks like React.js, I specialize in
+                  transforming designs into functional, accessible, and visually
+                  appealing interfaces. My focus is on creating seamless digital
+                  experiences that meet user needs and solve real-world
+                  problems.
+                </p>
+              </div>
+            </div>
+            <div className="px-10 max-[1100px]:px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">EXPERIENCES</h3>
+
+                <article className="py-4">
+                  <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                    <li className="py-1 px-5">
+                      <a
+                        className="underline"
+                        href="https://mobasheran.org/"
+                        target="_blank"
+                      >
+                        <strong>Front-End Developer</strong> at{" "}
+                        <strong>Mobasheran</strong>
+                      </a>
+                      <h4 className="font-extralight my-2 ml-3">
+                        Mar 2024 - Apr 2024 - Remote
+                      </h4>
+                      <ul>
+                        <li className="py-1 px-5 text-justify">
+                          Developed and optimized public pages and user panels
+                          using HTML, CSS, and Tailwind, resulting in a 25%
+                          improvement in site performance. Integrated
+                          accessibility features to enhance usability for a
+                          diverse range of users.
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </article>
+              </div>
+            </div>
+
+            <div className="px-10 max-[1100px]:px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">SELECTED PROJECT</h3>
+
+                <article className="py-4">
+                  <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                    <li className="py-1 px-5">
+                      <a
+                        className="underline"
+                        href="https://github.com/AbolfazlMahkam/CRM-Workroom"
+                        target="_blank"
+                      >
+                        <strong>CRM-Workroom</strong> Jan 2025
+                      </a>
+                      <ul>
+                        <li className="py-1 px-5 text-justify">
+                          Built a responsive web application inspired by a Figma
+                          design using React.js, TailwindCSS, and Vite. Added
+                          dark/light mode, dynamic data fetching with React
+                          Query and Axios, and served data locally via
+                          JSON-Server.
+                        </li>
+                      </ul>
+                      <>
+                        <h6 className="text-xs my-3">
+                          <b>Technologies I used</b>
+                        </h6>
+                        <div className="flex flex-row justify-around text-xs">
+                          <span className="cursor-pointer">ReactJS</span>
+                          <span className="cursor-pointer">TailwindCSS</span>
+                          <span className="cursor-pointer">Json-Server</span>
+                          <span className="cursor-pointer">React Query</span>
+                          <span className="cursor-pointer">Axios</span>
+                        </div>
+                      </>
+                    </li>
+                  </ul>
+                </article>
+                <article className="py-4">
+                  <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                    <li className="py-1 px-5">
+                      <a
+                        className="underline"
+                        href="https://github.com/AbolfazlMahkam/CRM-Workroom"
+                        target="_blank"
+                      >
+                        <strong>ToDo List</strong> Dec 2024
+                      </a>
+                      <ul>
+                        <li className="py-1 px-5 text-justify">
+                          Built a task management tool using HTML, CSS,
+                          TailwindCSS, and JavaScript. Integrated MongoDB for
+                          efficient data storage and dynamic task handling.
+                          Improved user productivity by 15% through seamless
+                          task categorization and drag-and-drop functionality.
+                        </li>
+                      </ul>
+                      <>
+                        <h6 className="text-xs my-3">
+                          <b>Technologies I used</b>
+                        </h6>
+                        <div className="flex flex-row justify-around text-xs">
+                          <span className="cursor-pointer">EJS</span>
+                          <span className="cursor-pointer">JavaScript</span>
+                          <span className="cursor-pointer">TailwindCSS</span>
+                          <span className="cursor-pointer">NodeJS</span>
+                          <span className="cursor-pointer">ExpressJS</span>
+                          <span className="cursor-pointer">MongoDB</span>
+                        </div>
+                      </>
+                    </li>
+                  </ul>
+                </article>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-2 max-[1100px]:hidden">
+            <div className="flex justify-center p-5">
+              <div className="rounded-lg border-2 border-red-700 p-1 hover:p-2 duration-300">
+                <img
+                  src="/img/me.jpg"
+                  className="aspect-auto rounded-md hover:shadow-xl shadow-black duration-300"
+                />
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-1 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">CONTACT</h3>
+                <ul className="dot-margin list-disc list-inside">
+                  <li className="py-1 px-5">
+                    <a
+                      className="underline"
+                      href="tel:+989104531958"
+                      target="_blank"
+                    >
+                      (+98)9104531958
+                    </a>
+                  </li>
+                  <li className="py-1 px-5">
+                    <a
+                      className="underline"
+                      href="mailto:a.mahkam.9500@gmail.com"
+                      target="_blank"
+                    >
+                      a.mahkam.9500@gmail.com
+                    </a>
+                  </li>
+
+                  <li className="flex flex-row justify-around py-3 px-5 max-w-xs">
+                    <a
+                      className="underline"
+                      href="https://github.com/AbolfazlMahkam/"
+                      target="_blank"
+                    >
+                      <img src="/img/github.png" width={25} />
+                    </a>
+                    <a
+                      className="underline"
+                      href="https://www.linkedin.com/in/abolfazl-mahkam/"
+                      target="_blank"
+                    >
+                      <img src="/img/linkedin.png" width={25} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">SKILLS</h3>
+                <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                  <li className="py-1 px-5">
+                    Full proficiency in HTML, CSS, JavaScript and TypeScript.
+                  </li>
+                  <li className="py-1 px-5">
+                    Proficiency ReactJS, NextJS, VueJS and NuxtJS.
+                  </li>
+                  <li className="py-1 px-5">
+                    Skilled in TailwindCSS, Bootstrap, and responsive design
+                    principles.
+                  </li>
+                  <li className="py-1 px-5">
+                    Proficient in Node.js and MongoDB.
+                  </li>
+                  <li className="py-1 px-5">
+                    Proficiency in Git, Figma, AdobeXD, and collaborative
+                    workflows.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">LANGUAGES</h3>
+                <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                  <li className="py-1 px-5">Persian - Native</li>
+                  <li className="py-1 px-5">English - Intermediate(B1)</li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-5">
+              <div className="mx-auto border-l-8 border-red-700 my-7 rounded-lg josefinsans">
+                <h3 className="p-4 text-2xl text-red-600">EDUCATION</h3>
+                <ul className="dot-margin list-disc list-inside text-sm align-top font-sans">
+                  <div className="p-4 pl-6">
+                    <h1 className="font-semibold text-xl">
+                      Isalmic Azad University
+                    </h1>
+                    <h4 className="font-extralight">Oct 2023 - present</h4>
+                    <h3>
+                      Pursuing a Bachelor’s Degree in Computer Engineering
+                    </h3>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </main>
+  );
+};
+
+export default Resume;
+
+export const Head: HeadFC = () => <title>Resume</title>;
