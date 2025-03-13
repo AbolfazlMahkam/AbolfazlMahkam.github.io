0bf1a5fca87232aa65ee0cf57690236ce06fb674
a.mahkam.950 <a.mahkam.9500@gmail.com>
2025-03-13 15:15:04 +0330

    ud

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,34 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Home from "./Home";
-import { Flex, Spin } from "antd";
+// import { Flex, Spin } from "antd";
 import "../styles/global.css";
 
 const IndexPage: React.FC<PageProps> = (props) => {
-  const [loading, setLoading] = React.useState(true);
-  const [percent, setPercent] = React.useState(-50);
-  let timerId: ReturnType<typeof setTimeout>;
+  // const [loading, setLoading] = React.useState(true);
+  // const [percent, setPercent] = React.useState(-50);
+  // let timerId: ReturnType<typeof setTimeout>;
 
-  React.useEffect(() => {
-    timerId = setTimeout(() => {
-      setPercent((v) => {
-        const nextPercent = v + 5;
-        return nextPercent > 150 ? -50 : nextPercent;
-      });
-    }, 100);
-    return () => clearTimeout(timerId);
-  }, [percent]);
+  // React.useEffect(() => {
+  //   timerId = setTimeout(() => {
+  //     setPercent((v) => {
+  //       const nextPercent = v + 5;
+  //       return nextPercent > 150 ? -50 : nextPercent;
+  //     });
+  //   }, 100);
+  //   return () => clearTimeout(timerId);
+  // }, [percent]);
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2500);
-  }, []);
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2500);
+  // }, []);
 
   return (
     <>
       <main className="bg-black">
-        {loading ? (
+        {/* {loading ? (
           <Flex align="center" justify="center" style={{ height: "100vh" }}>
             <Spin
               size="large"
@@ -37,7 +37,8 @@ const IndexPage: React.FC<PageProps> = (props) => {
           </Flex>
         ) : (
           <Home {...props} />
-        )}
+          )} */}
+        <Home {...props} />
       </main>
     </>
   );
